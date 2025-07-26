da90d110773166a74b7004d21a955d100f9965c7
lromero <lromero@lcg.unam.mx>
2025-07-25 18:54:14 -0600

    feat: update parameter class weight logic of the model

--- code/train.py
+++ code/train.py
@@ -2,11 +2,13 @@ import argparse
 import os
 import pandas as pd
 import joblib
+import json
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score
 
 def main():
-    # Argumentos
+    
+    # Arguments
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--output-data-dir', type=str, default=os.environ.get('SM_OUTPUT_DATA_DIR'))
@@ -26,25 +28,34 @@ def main():
     X_train = train_data.iloc[:, :-1]
     y_train = train_data.iloc[:, -1]
 
-    # Entrenar modelo
+    # Training model
+    class_weight = args.class_weight
+    if (class_weight != "balanced") or (class_weight is not None):
+        class_weight = json.loads(class_weight)
+
     model = RandomForestClassifier(
         n_estimators=args.n_estimators,
         max_depth=args.max_depth,
-        class_weight=args.class_weight,
+        class_weight=class_weight,
         min_samples_split=args.min_samples_split
     )
     model.fit(X_train, y_train)
 
-    # Evaluar si hay datos de validación
+    # Evaluate model if validation data exist
     if args.validation and os.path.exists(os.path.join(args.validation, 'validation.csv')):
+        
         val_data = pd.read_csv(os.path.join(args.validation, 'validation.csv'), header=None)
+
         X_val = val_data.iloc[:, :-1]
         y_val = val_data.iloc[:, -1]
+
         predictions = model.predict(X_val)
+
         f1 = round(f1_score(y_val, predictions, average="macro"), 4)
+
         print(f"F1-score={f1}")
 
-    # Guardar modelo
+    # Saving model
     joblib.dump(model, os.path.join(args.model_dir, "model.joblib"))
 
 if __name__ == '__main__':
