10ddd8cc92aeb95fd5ca9894e9fa3fb9b0105468
lromero <lromero@lcg.unam.mx>
2025-07-27 15:04:41 -0600

    chore/fix: add new log (info/debug) and fix class_weight value

--- code/train.py
+++ code/train.py
@@ -1,13 +1,24 @@
 import argparse
 import os
-import pandas as pd
 import joblib
 import json
+import logging
+
+import pandas as pd
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score
 
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
+
+
 def main():
     
+    logger.info("Starting training process")
+
     # Arguments
     parser = argparse.ArgumentParser()
 
@@ -23,25 +34,32 @@ def main():
 
     args = parser.parse_args()
 
+    logger.info("Loading training data...")
+
     # Loading training data
     train_data = pd.read_csv(os.path.join(args.train, 'train.csv'), header=None)
     X_train = train_data.iloc[:, :-1]
     y_train = train_data.iloc[:, -1]
 
-    # Training model
+    # Defining right format for parameter class_weight
     class_weight = args.class_weight
     if (class_weight != "balanced") or (class_weight is not None):
         class_weight = json.loads(class_weight)
+        class_weight = {int(key):float(value) for key, value in class_weight.items()}
+
+    logger.info("\nTraining model\n")
 
     model = RandomForestClassifier(
         n_estimators=args.n_estimators,
         max_depth=args.max_depth,
         class_weight=class_weight,
-        min_samples_split=args.min_samples_split
+        min_samples_split=args.min_samples_split,
+        verbose=1
     )
     model.fit(X_train, y_train)
 
     # Evaluate model if validation data exist
+    logger.info("Using validation dataset...")
     if args.validation and os.path.exists(os.path.join(args.validation, 'validation.csv')):
         
         val_data = pd.read_csv(os.path.join(args.validation, 'validation.csv'), header=None)
@@ -53,10 +71,14 @@ def main():
 
         f1 = round(f1_score(y_val, predictions, average="macro"), 4)
 
+        logger.debug(f"F1-score={f1}")
         print(f"F1-score={f1}")
 
     # Saving model
+    logger.info("Saving trained model")
     joblib.dump(model, os.path.join(args.model_dir, "model.joblib"))
+
+    logger.info("All done!")
 
 if __name__ == '__main__':
     main()
