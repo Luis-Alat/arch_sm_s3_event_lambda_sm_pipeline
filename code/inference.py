15c8a1785f1c96fb659eae1265a630f9893d0610
lromero <lromero@lcg.unam.mx>
2025-07-28 17:33:12 -0600

    feat: add transformer logic to the endpoint and aditional validations

--- code/inference.py
+++ code/inference.py
@@ -1,13 +1,37 @@
-import joblib
 import os
+import io
+import joblib
+import pandas as pd
 
-# Load Model
+
 def model_fn(model_dir):
-    model_path = os.path.join(model_dir, "model.joblib")
-    model = joblib.load(model_path)
-    return model
+    model = joblib.load(os.path.join(model_dir, "model.joblib"))
+    transformer = joblib.load(os.path.join(model_dir, "transformer.joblib"))
+    return {"model": model, "transformer": transformer}
 
-# Call Model
-def predict_fn(input_data, model):
-    return model.predict(input_data)
+
+def input_fn(input_data, content_type):
+    if content_type == "text/csv":
+        return pd.read_csv(io.StringIO(input_data))
+    else:
+        raise ValueError(f"Unsupported content type: {content_type}")
+
+
+def predict_fn(input_data, model_bundle):
+    transformer = model_bundle["transformer"]
+    model = model_bundle["model"]
+
+    processed_data = transformer.transform(input_data)
+
+    prediction = model.predict(processed_data)
+    
+    return prediction
+
+
+def output_fn(prediction, accept):
+    if accept == "text/csv":
+        return ",".join(str(x) for x in prediction)
+    else:
+        raise ValueError(f"Unsupported accept type: {accept}")
+
 
