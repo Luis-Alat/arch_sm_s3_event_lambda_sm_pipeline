20e2d82bfe7e7d69351e72afd5817ff99c204aff
Luis Romero <angel.odct.1@gmail.com>
2025-08-04 18:59:16 -0600

    feat: add API Gateway stack code into app.py

---
+++ cdk/api_stack.py
@@ -0,0 +1,18 @@
+from aws_cdk import Stack, aws_apigateway
+from constructs import Construct
+
+class CdkApi(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, *, lambda_function, **kwargs):
+        
+        super().__init__(scope, construct_id, **kwargs)
+
+        api = aws_apigateway.RestApi(
+            self,
+            "RestApiGetPrediction",
+            rest_api_name="GetPrediction",
+        )
+        api.root.add_resource("predict").add_method(
+            "POST",
+            aws_apigateway.LambdaIntegration(lambda_function)
+        )
