d30dd5265e7017681eed56445167132245538574
Luis Romero <angel.odct.1@gmail.com>
2025-08-06 17:15:05 -0600

    feat: create eventBridge rule to trigger pipeline execution using CDK

--- cdk/app.py
+++ cdk/app.py
@@ -3,11 +3,13 @@ import aws_cdk as cdk
 from lambdas.lambda_stack import CdkLambdaStack
 from api.api_stack import CdkApiStack
 from pipeline.sagemaker_pipeline_stack import CdkSagemakerPipelinesStack
+from event.event_bridge_stack import CdkEventStack
 
 app = cdk.App()
 
 lambda_stack = CdkLambdaStack(app, "LambdaStack")
 api_stack = CdkApiStack(app, "ApiStack", lambda_function=lambda_stack.lambda_create_end)
 sm_pipe_stack = CdkSagemakerPipelinesStack(app, "SagemakerPipelineStack")
+event_rule = CdkEventStack(app, "EventBridgeRuleStack")
 
 app.synth()
