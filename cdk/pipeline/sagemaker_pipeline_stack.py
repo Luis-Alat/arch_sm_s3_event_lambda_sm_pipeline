76a2d403f04cd2b56d3ff2408d351f7210170f72
Luis Romero <angel.odct.1@gmail.com>
2025-08-06 13:45:19 -0600

    fix

--- cdk/pipeline/sagemaker_pipeline_stack.py
+++ cdk/pipeline/sagemaker_pipeline_stack.py
@@ -9,28 +9,23 @@ class CdkSagemakerPipelinesStack(Stack):
 
         super().__init__(scope, id, **kwargs)
 
-        # 1. Load the JSON file into a Python dictionary.
-        with open("pipeline/sagemaker/loan_default_pipeline.json", "r") as f:
-            pipeline_definition_dict = json.load(f)
 
-        # 2. Convert the Python dictionary to a JSON string.
-        # This is the crucial step to satisfy the 'str' type requirement.
-        pipeline_definition_json_string = json.dumps(pipeline_definition_dict)
+        with open("pipeline/sagemaker/loan_default_pipeline.json", "r") as f:
+            pipeline_definition = f.read()
+
 
         pipeline_name = "PipelineSkLernLoanDefault"
         pipeline_role_arn = "arn:aws:iam::007863746889:role/sagemakerS3"
 
-        # 3. Create a CfnPipeline.PipelineDefinitionProperty object.
-        # This object's 'pipeline_definition_body' property explicitly expects a string.
         pipeline_definition_property = aws_sagemaker.CfnPipeline.PipelineDefinitionProperty(
-            pipeline_definition_body=pipeline_definition_json_string
+            pipeline_definition_body=pipeline_definition
         )
 
-        # 4. Create the CfnPipeline resource and pass the property object.
+
         self.sm_pipeline = aws_sagemaker.CfnPipeline(
             self,
             pipeline_name,
             pipeline_name=pipeline_name,
             role_arn=pipeline_role_arn,
-            pipeline_definition=pipeline_definition_json_string,
+            pipeline_definition=pipeline_definition_property,
         )
