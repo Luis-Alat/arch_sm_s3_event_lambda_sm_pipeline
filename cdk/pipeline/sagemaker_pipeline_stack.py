7b2133c4ceda47c32d0e8e0832908cf68d774f4b
Luis Romero <angel.odct.1@gmail.com>
2025-08-06 12:59:20 -0600

    fix: sagemaker pipeline definition now passed directly to yhe stack instead in a dict

--- cdk/pipeline/sagemaker_pipeline_stack.py
+++ cdk/pipeline/sagemaker_pipeline_stack.py
@@ -19,5 +19,5 @@ class CdkSagemakerPipelinesStack(Stack):
             pipeline_name,
             pipeline_name=pipeline_name,
             role_arn=pipeline_role_arn,
-            pipeline_definition={"pipelineDefinition": pipeline_definition},
+            pipeline_definition=pipeline_definition,
         )
