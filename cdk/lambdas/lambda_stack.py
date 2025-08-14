d58cd337715457435095cc38ac0295fd7defa8ca
Luis Romero <angel.odct.1@gmail.com>
2025-08-13 19:14:04 -0600

    fix

--- cdk/lambdas/lambda_stack.py
+++ cdk/lambdas/lambda_stack.py
@@ -36,7 +36,7 @@ class CdkLambdaStack(Stack):
             code=_lambda.Code.from_asset("lambdas/call_endpoint"),
             role=invocation_lambda_role,
             function_name="CallEndpoint",
-            timeout=Duration.seconds(6)
+            timeout=Duration.seconds(10)
         )
 
         self.lambda_create_end = _lambda.Function(
